5db51c018dee5e89512938c78181fb723cc4a1a4
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-02-25 01:54:35 -0500

    cannot assign twice to immutable varaible

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ fn main() {
 
 
     let a: u8 = 255; // - u8 unsined integer, which means that the variable a can only be between 0 and 255
-    println!("{}", a)
+    println!("a = {}", a)
+
+    a = 40;
 
 }
