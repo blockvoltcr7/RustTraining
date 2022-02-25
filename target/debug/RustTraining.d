b0bbc885d0ff15c9894e7bf2e04aa814d348cfc2
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-02-25 00:29:04 -0500

    Changes

---
+++ target/debug/RustTraining.d
@@ -0,0 +1,1 @@
+/Users/samisabir-idrissi/code/rust/RustTraining/target/debug/RustTraining: /Users/samisabir-idrissi/code/rust/RustTraining/src/main.rs
---
+++ target/debug/deps/RustTraining-09d82338bbd4cdc0.d
@@ -0,0 +1,5 @@
+/Users/samisabir-idrissi/code/rust/RustTraining/target/debug/deps/RustTraining-09d82338bbd4cdc0: src/main.rs
+
+/Users/samisabir-idrissi/code/rust/RustTraining/target/debug/deps/RustTraining-09d82338bbd4cdc0.d: src/main.rs
+
+src/main.rs:
