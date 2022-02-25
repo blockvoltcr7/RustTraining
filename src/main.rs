1f2410624456999ea434401e621868b512a578c6
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-02-25 02:14:14 -0500

    getting the size of the value by operating system

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,12 @@ use std::mem;
 
 fn main() {
 
-    let c = 12345689; //i32 = 32bits = 4 bytes
-
-    println!("c = {}, takes up {} bytes", c, mem::size_of_val(&c));
-
-    //c has a reference sign infront if it because we want to know the bytes
-
-    //u8, u16, u32, u64, i8, i16, .... all used to declare integers
-
+    let z: isize = 123;
+    let size_of_z = mem::size_of_val(&z);
+    println!(
+        "z = {}, takes up {} bytes, {}-bit OS",
+             z, size_of_z, size_of_z*8
+    );
 
 
 }
