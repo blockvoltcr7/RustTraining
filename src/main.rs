e8215cb8f78cabb197668ce99c92620499063cc8
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-02-25 00:55:27 -0500

    getting length and using assert

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
 
 
-    let method = b"GET";
-    assert_eq!(method, &[b'G',b'E',b'T'])
-    /**
-    A string literal witht he b prefix is a byte string.
-    reference array of three bytes that equal the word GET
-    so we are asserting if each reference character equals GET
-    */
+    let var = "tesla";
+    println!("{}",var.len());
+    assert_eq!(var.len(),5);
+    println!("passed")
+
 }
