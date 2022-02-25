2bfe3b22162c14772b9f5075aee475696851c788
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-02-25 02:38:16 -0500

    cant access variable outside its scope function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 use std::mem;
 
+fn func(){
+    let a = 8;
+
+}
+
 fn main() {
 
-    //bitwise
-
-    let c = 1 | 2;
-
-    println!("1|2 = {}", c);
-
-    let x = 2;
-    let y = 3;
-
-    let bool = x<y;
-
-    println!("x is less than y : {}", bool)
+    println!("{}", a)
 
 }
