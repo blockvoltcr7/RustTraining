5c13a48c5851c4689e80b63b7e1b487a85c36721
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-02-25 00:40:53 -0500

    string variable assigment and printout as string literal

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    println!("Hello, world!");
 
-
+    let speech = "hey sami";
+    // eprintln!(speech) -- you cant print it like this, it has to be a literal
+    println!("{}", speech) //correct way to print a string
 
 }
