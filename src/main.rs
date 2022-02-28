d4a0ca12bb7d1458a8890bdd0853b58813c07e81
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-02-27 23:40:35 -0500

    arrays basics, matric array, loop over array

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,64 @@
 
-
 use std::mem;
-enum Color {
-    RED,
-    GREEN,
-    BLUE,
-    RgbColor(u8,u8,u8), //tuple structure object
-    CmykColor{ //struct object that holds 4 u8 date type objects
-        cyan:u8,
-        magenta:u8,
-        yellow:u8,
-        black:u8
-    },
-
-}
-
-
-fn enums(){
-    let c = Color::CmykColor {cyan:0, magenta:128, yellow:0, black:255};
-
-    match c { //match is like a switch case statement
-        Color::RED => println!("red"),
-        Color::GREEN => println!("green"),
-        Color::BLUE => println!("blue"),
-        Color::RgbColor(0, 0, 0)
-        | Color::CmykColor {
-            cyan: _,
-            magenta: 128,
-            yellow: _,
-            black: 255,
-        } => println!("black"),
-        Color::RgbColor(r, g, b) => println!("rgb({},{},{}", r, g, b),
-        _ => (),
-
-    };
-}
 
 fn main() {
-    enums();
+
+    //mutable array that has 5 mutable positions
+    let mut a:[i32;5] = [1,2,3,4,5];
+    //let mut a = [1,2,3,4,5]; //can also be written like this with specifyin the the data type
+
+    a[0] = 123;
+    println!("a has {} elements, first is {}",a.len(), a[0]);
+
+    println!("{:?}",a); //print all contents of an array
+
+    if a == [1,2,3,4,5] {
+        println!("match")
+    }else{
+        println!("does not match")
+    }
+
+    let b = [1;10]; //ten elements that are all 1's
+    let r = [1u16;10]; //control the size of of bytes for a variable
+
+    for x in b{
+        println!("{}",x);
+    }
+
+    //another way to write the for loop
+    for i in 5..b.len(){
+        println!("other for loop {}", i);
+
+    }
+
+    //print out to see how much memory was taken in the array
+    println!("b took up {} bytes", mem::size_of_val(&b));
+    println!("a took up {} bytes", mem::size_of_val(&a));
+    println!("r took up {} bytes", mem::size_of_val(&r));
+
+    //multidimensional array
+
+    let mtx:[[f32;3];3] =
+    [
+        [1.0, 0.0, 0.0],
+        [0.0, 2.0, 0.1],
+        [0.3, 2.0, 0.2]
+
+    ];
+    println!("{:?}", mtx);
+    println!("{:?}", mtx[1][1]);
+
+    println!("matrix length is {}", mtx.len());
+    for i in 0..mtx.len(){
+        for j in 0..mtx[i].len(){
+            if i == j{
+                println!("match found mtx [{}][{}] = {}", i, j, mtx[i][j]);
+            }
+            else {
+                println!("no match mtx [{}][{}] = {}", i, j, mtx[i][j]);
+            }
+        }
+    }
+
 }
 
