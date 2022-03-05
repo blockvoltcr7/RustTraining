83623e86517e2783b71c6e3a5aa306bc59cef650
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-03-05 17:34:36 -0500

    return statement example

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::collections::HashSet;
 
 
-fn increase (x: &mut i32){
-    *x += 1;
+fn multiply(x:i32, y:i32) -> i32{ //arrow symbol is used to specify return statement, must specify data type being returned
+
+    x * y //return statement does not require a semi colon
+
 }
 
 fn main() {
 
-    let mut num = 1;
-    increase(&mut num);
-    println!("{}", num);
+    let a = 5;
+    let b = 5;
+
+    let num = multiply(a,b);
+
+    println!("{}", num)
 
 }
 
