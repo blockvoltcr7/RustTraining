0ea119fa9746a4e94f6a34a27192802bdb5b6991
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-03-05 18:12:17 -0500

    using closures with references and dereferencing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-use std::collections::HashSet;
 
 
-fn multiply(x:i32, y:i32) -> i32{ //arrow symbol is used to specify return statement, must specify data type being returned
 
-    x * y //return statement does not require a semi colon
 
-}
-
+/**
+    basically passing a func to a variable then calling it
+ */
 fn main() {
 
-    let a = 5;
-    let b = 5;
-
-    let num = multiply(a,b);
-
-    println!("{}", num)
+    closures()
 
 }
 
+fn closures(){
+
+    let plus_one = |x:i32| -> i32 {x + 1};
+    let a = 7;
+    println!("{} {}",a, plus_one(a));
+
+    let mut two = 2;
+    {
+        let plus_two = |x| {
+            let mut z = x;
+            z += two;
+            z
+        };
+        println!("{} + 2 = {}", 3, plus_two(3));
+
+    }
+    let borrow_two = &mut two;
+
+    //T: by value
+    //T& by refrence
+    // &mut &
+
+    let plus_three = |x:&mut i32| *x += 3;
+
+    let mut f = 12;
+    plus_three(&mut f);
+    println!("f = {}", f);
+
+}
