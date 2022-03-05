6989fcf28fb4a60bdb4ef07761cd075439a2f1f4
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-03-05 17:31:23 -0500

    mut param function referencing then dereferencing

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,15 @@
 use std::collections::HashSet;
 
+
+fn increase (x: &mut i32){
+    *x += 1;
+}
+
 fn main() {
 
-    //iterations
-
-    let mut vec = vec![3,2,1];
-
-    for x in vec.iter() {
-        println!("{} ", x)
-    }
-
-    for x in vec.iter_mut() {
-        *x +=2;
-        println!("{} ", x)
-    }
-
-    println!("{:?}", vec);
-
-    //print reverse order
-    for x in vec.iter_mut().rev() {
-        *x +=2;
-        println!(" in reverse {} ", x)
-    }
-
-
-
+    let mut num = 1;
+    increase(&mut num);
+    println!("{}", num);
 
 }
 
