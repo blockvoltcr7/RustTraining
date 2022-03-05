3f6fd451c4638f7178060d8419888b04b8b2c9cc
sami sabir-idrissi <samisabiridrissi@icloud.com>
2022-03-05 17:20:09 -0500

    basics of iterator

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,29 @@ use std::collections::HashSet;
 
 fn main() {
 
+    //iterations
 
-    let mut countries = HashSet::new();
+    let mut vec = vec![3,2,1];
 
-    //hashset only stores unique elements
-    countries.insert("morocco");
-    countries.insert("slovakia");
-    countries.insert("slovakia");
-
-    println!("{:?}", countries);
-
-    let added = countries.insert("brazil");
-
-    if added {
-        println!("{}", added);
+    for x in vec.iter() {
+        println!("{} ", x)
     }
 
-    //check if hashset has a value
+    for x in vec.iter_mut() {
+        *x +=2;
+        println!("{} ", x)
+    }
 
-    if !countries.contains("brazil") {
-        println!("we dont have brazil");
-    }else{
-        println!("we do have brazil");
+    println!("{:?}", vec);
 
+    //print reverse order
+    for x in vec.iter_mut().rev() {
+        *x +=2;
+        println!(" in reverse {} ", x)
     }
 
 
-    //remove an element from hashset
-    let removed = countries.remove("brazil");
-    if removed {
-        println!("we removed brazil")
-    }
 
-
-    let _1_5: HashSet<_> =  ( 1..=5).collect();
-    let _6_10: HashSet<_> =  ( 6..=10).collect();
-    let _1_10: HashSet<_> =  ( 1..=10).collect();
-    let _2_8: HashSet<_> =  ( 2..=8).collect();
-
-    //subset, every element is contain in the other set
-    println!("is {:?} a subset of {:?} ? {}", _2_8, _1_10, _2_8.is_subset(&_1_10))
 
 }
 
